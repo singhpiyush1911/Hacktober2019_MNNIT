c2ae6fa8df71667341627802730ea7597b76e67b
singhpiyush1911 <48802957+singhpiyush1911@users.noreply.github.com>
2019-10-01 21:59:35 +0530

    Update swap_2_numbers.c

--- swap_2_numbers.c
+++ swap_2_numbers.c
@@ -9,7 +9,7 @@ int main()
       // Value of firstNumber is assigned to temporaryVariable
       temporaryVariable = firstNumber;
       // Value of secondNumber is assigned to firstNumber
-      firstNumber = firstNumber;
+      firstNumber = secondNumber;
       // Value of temporaryVariable (which contains the initial value of firstNumber) is assigned to secondNumber
       secondNumber = temporaryVariable;
       printf("\nAfter swapping, firstNumber = %s\n", firstNumber);
